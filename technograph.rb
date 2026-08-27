e0ac14f31e0d2d1f659b9c7d14b89cc7e794ec59
b1rd33 <christian@tabulara.de>
2026-08-27 11:18:33 +0300

    technograph: build v0.1.0 from source

--- technograph.rb
+++ technograph.rb
@@ -4,33 +4,20 @@
 class Technograph < Formula
   desc "Conservative HTTP-only technographic detection CLI"
   homepage "https://github.com/b1rd33/technograph"
-  version "0.1.0"
+  url "https://github.com/b1rd33/technograph/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "64781b203c469004925687e74cff4ef8f9a82aa03de6cb07b9035ed65fbea99a"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/b1rd33/technograph/releases/download/v0.1.0/technograph_0.1.0_darwin_x86_64.tar.gz"
-      sha256 "54a938b8b648d0a40da4cb53c9253727ec7c466d64d6edd6eddc7edf07104007"
-    end
-    if Hardware::CPU.arm?
-      url "https://github.com/b1rd33/technograph/releases/download/v0.1.0/technograph_0.1.0_darwin_arm64.tar.gz"
-      sha256 "48606ff4c71ace45aa917eb98cded5291c1e54863cc1949db631ee74ba6abe49"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/b1rd33/technograph/releases/download/v0.1.0/technograph_0.1.0_linux_x86_64.tar.gz"
-      sha256 "707c656a123ca2c4dbfaa61afb4b789097af68a9a93ed78fdc35f7e652b78521"
-    end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/b1rd33/technograph/releases/download/v0.1.0/technograph_0.1.0_linux_arm64.tar.gz"
-      sha256 "02775fec6ce277ed8e950aae1b6b6c2b34aed84aa5b1a224dac49e363873e6c6"
-    end
-  end
+  depends_on "go" => :build
 
   def install
-    bin.install "technograph"
+    ldflags = %W[
+      -s -w
+      -X github.com/b1rd33/technograph/internal/buildinfo.Version=#{version}
+      -X github.com/b1rd33/technograph/internal/buildinfo.Commit=395b011
+      -X github.com/b1rd33/technograph/internal/buildinfo.Date=2026-08-27T08:08:38Z
+    ]
+    system "go", "build", "-trimpath", "-ldflags", ldflags.join(" "), "-o", bin/"technograph", "./cmd/technograph"
   end
 
   test do
