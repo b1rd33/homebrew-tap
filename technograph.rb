44e2e29e0993e0698142d7f6deb2f92d12a9fc02
b1rd33 <christian@tabulara.de>
2026-08-30 18:15:33 +0300

    Update technograph to 1.1.0

--- technograph.rb
+++ technograph.rb
@@ -5,8 +5,8 @@
 class Technograph < Formula
   desc "Conservative HTTP-only technographic detection CLI"
   homepage "https://github.com/b1rd33/technograph"
-  url "https://github.com/b1rd33/technograph/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "cf6c0dbeab68577c97205ccba7128c0b2efaa508f602927b5936e9d889293325"
+  url "https://github.com/b1rd33/technograph/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "28f5e165aa0c51ebc72a7617fed29033dfc11faaa08701ed1a5d3812623eaa49"
   license "MIT"
 
   depends_on "go" => :build
@@ -15,8 +15,8 @@ class Technograph < Formula
     ldflags = %W[
       -s -w
       -X github.com/b1rd33/technograph/internal/buildinfo.Version=#{version}
-      -X github.com/b1rd33/technograph/internal/buildinfo.Commit=00dc1d5
-      -X github.com/b1rd33/technograph/internal/buildinfo.Date=2026-08-30T08:45:47Z
+      -X github.com/b1rd33/technograph/internal/buildinfo.Commit=823ec93
+      -X github.com/b1rd33/technograph/internal/buildinfo.Date=2026-08-30T15:13:12Z
     ]
     system "go", "build", "-trimpath", "-ldflags", ldflags.join(" "), "-o", bin/"technograph", "./cmd/technograph"
     system "go", "build", "-trimpath", "-ldflags", ldflags.join(" "),
